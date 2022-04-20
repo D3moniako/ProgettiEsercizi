de3d263771206c85d3647e1539babf4b4306afa2
D3moniako <soleroboo@gmail.com>
2022-04-20 13:07:31 +0200

    progetto

---
+++ PROGETTI/Editoriale/src/main/java/com/Editoriale/Editoriale/EditorialeApplication.java
@@ -0,0 +1,13 @@
+package com.Editoriale.Editoriale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorialeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorialeApplication.class, args);
+	}
+
+}
---
+++ PROGETTI/Editoriale/src/test/java/com/Editoriale/Editoriale/EditorialeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Editoriale.Editoriale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorialeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
